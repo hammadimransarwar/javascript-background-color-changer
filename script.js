be3ef91388749fbf7a6dd6158660299ceaf07dac
Muhammad Hammad Imran <hammad.16.imran@gmail.com>
2026-06-26 19:51:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let b=document.querySelector('.blue')
+b.addEventListener("click",function(){
+    document.body.style.backgroundColor="blue"
+})
+let g=document.querySelector('.green')
+g.addEventListener("click",function(){
+    document.body.style.backgroundColor="green"
+})
+let o=document.querySelector('.orange')
+o.addEventListener("click",function(){
+    document.body.style.backgroundColor="orange"
+})
+let r=document.querySelector('.reset')
+r.addEventListener("click",function(){
+    document.body.style.backgroundColor="white"
+})
